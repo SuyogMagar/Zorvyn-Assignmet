be0d5183a33e12b55ba18898de8c7aba5aba3e4e
SuyogMagar <hkmagar11@gmail.com>
2026-04-06 00:24:44 +0530

    Updated API Documentaion (Postman Requests) & Updated ExceptionHandler class

--- src/main/java/org/example/suyog_zorvynassignment/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/suyog_zorvynassignment/exception/GlobalExceptionHandler.java
@@ -46,11 +46,19 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(org.springframework.web.servlet.resource.NoResourceFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoResourceFoundException(org.springframework.web.servlet.resource.NoResourceFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Not Found");
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleGeneralException(Exception ex) {
         ex.printStackTrace();
         Map<String, String> response = new HashMap<>();
-        response.put("error", "Internal Server Error ( Analyst or Viewer Can't Create a Record )");
+        response.put("error", "Internal Server Error");
         response.put("message", ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
